eaad0636513b60c28f396d7d6b17f0958167344a
Varvara <zolotykh2020@gmail.com>
2023-03-30 10:19:16 +0200

    delete-inserted

--- posts/main.js
+++ posts/main.js
@@ -1,47 +1,49 @@
-const formE1 = document.querySelector('.form');
+function darkmode() {
+    const body = document.querySelector("body");
+    (sidebar = body.querySelector(".sidebar")),
+        (toggle = body.querySelector(".toggle")),
+        (searchBtn = body.querySelector(".seach-box")),
+        (modeSwitch = body.querySelector(".toggle-switch")),
+        (modeText = body.querySelector(".mode-text"));
 
-formE1.addEventListener('submit', event => {
-    event.preventDefault();
+    modeSwitch.addEventListener("click", () => {
+        sidebar.classList.toggle("dark");
+    });
 
-    const formData = new FormData(formE1);
-    const data = Object.fromEntries(formData);
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+    });
+}
 
-    fetch('http://127.0.0.1:5000/posts', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-});
+function insert () {
+    const formE1 = document.querySelector('.form');
 
-function get() {
-  fetch("http://127.0.0.1:5000/posts").then((r) => {
-      if (r.ok) {
-          r.json().then((v) => {
-              var comments = v.comments;
-              var container = document.getElementById("container-comment");
-              for (let i in comments) {
-                  container.innerHTML +=
-                      `
-                      <div class="one-post" id="` +
-                      comments[i].id +
-                      `">
-                          <p>` +
-                      comments[i].content +
-                      `</p>
-                          <p style="text-align: left; font-style: italic;">` +
-                      comments[i].user +
-                      ` <span style="text-align: right;">` +
-                      comments[i].time +
-                      `</span></p>
-                          </div>`;
-              }
-          });
-      }
-  });
+    formE1.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formData = new FormData(formE1);
+        const data = Object.fromEntries(formData);
+
+        fetch('http://127.0.0.1:5000/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+    });
+}
+
+function width() {
+    var w = window.innerWidth-250;
+    if (w < 300) {
+        w = 300;
+    } else if (w > 1000) {
+        w = 1000;
+    }
+    return w;
 }
 
 function removeAllChildNodes(parent) {
@@ -55,6 +57,43 @@ function remove() {
     removeAllChildNodes(container);
 }
 
+function deletePost(id) {
+    data = {'id': id}
+    fetch('http://127.0.0.1:5000/posts', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    remove()
+    get()
+}
+
+function get() {
+  fetch("http://127.0.0.1:5000/posts").then((r) => {
+      if (r.ok) {
+          r.json().then((v) => {
+              var comments = v.comments;
+              var container = document.getElementById("container-comment");
+              var length = comments.length-1
+              comments.reverse()
+              for (let i in comments) {
+                    container.innerHTML +=
+                    `<div class="one-post" style="width: ` + width() + `px" id="id` + comments[i].id + `">
+                        <button onclick="deletePost(this.id)" id="` + comments[i].id + `"><img src="remove.png" width="30px"></button>
+                        <p>` + comments[i].content + `</p>
+                        <p class="user-time" style="text-align: left;` + (width()-40) + `px;">` + comments[i].user + `
+                            <span style="text-align: right;">` + comments[i].time + `</span>
+                        </p>
+                    </div>`;
+              }
+          });
+      }
+  });
+}
+
+darkmode()
 get()
 var intervalId = window.setInterval(function(){
     remove()

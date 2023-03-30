c13c78c2379180652d4e6b9f5194ecab0d58f5d4
Varvara <zolotykh2020@gmail.com>
2023-03-30 11:58:50 +0200

    delete-fertig

--- text-field/main.js
+++ text-field/main.js
@@ -1,22 +1,18 @@
-function insert() {
-    const formE1 = document.querySelector('.form');
+const formE1 = document.querySelector('.form');
 
-    formE1.addEventListener('submit', event => {
-        event.preventDefault();
+formE1.addEventListener('submit', event => {
+    event.preventDefault();
 
-        const formData = new FormData(formE1);
-        const data = Object.fromEntries(formData);
+    const formData = new FormData(formE1);
+    const data = Object.fromEntries(formData);
 
-        fetch('http://127.0.0.1:5000/posts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then(res => res.json())
-            .then(data => console.log(data))
-            .catch(error => console.log(error));
-    });
-    remove()
-    get()
-}
+    fetch('http://127.0.0.1:5000/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});

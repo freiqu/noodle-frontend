5eaa0286650408b91b06e8aebe5e7de651f5cb6a
Varvara <zolotykh2020@gmail.com>
2023-03-30 10:42:37 +0200

    delete-fertig

--- posts/main.js
+++ posts/main.js
@@ -1,41 +1,3 @@
-function darkmode() {
-    const body = document.querySelector("body");
-    (sidebar = body.querySelector(".sidebar")),
-        (toggle = body.querySelector(".toggle")),
-        (searchBtn = body.querySelector(".seach-box")),
-        (modeSwitch = body.querySelector(".toggle-switch")),
-        (modeText = body.querySelector(".mode-text"));
-
-    modeSwitch.addEventListener("click", () => {
-        sidebar.classList.toggle("dark");
-    });
-
-    modeSwitch.addEventListener("click", () => {
-        body.classList.toggle("dark");
-    });
-}
-
-function insert () {
-    const formE1 = document.querySelector('.form');
-
-    formE1.addEventListener('submit', event => {
-        event.preventDefault();
-
-        const formData = new FormData(formE1);
-        const data = Object.fromEntries(formData);
-
-        fetch('http://127.0.0.1:5000/posts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then(res => res.json())
-            .then(data => console.log(data))
-            .catch(error => console.log(error));
-    });
-}
-
 function width() {
     var w = window.innerWidth-250;
     if (w < 300) {
@@ -57,26 +19,12 @@ function remove() {
     removeAllChildNodes(container);
 }
 
-function deletePost(id) {
-    data = {'id': id}
-    fetch('http://127.0.0.1:5000/posts', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    remove()
-    get()
-}
-
 function get() {
   fetch("http://127.0.0.1:5000/posts").then((r) => {
       if (r.ok) {
           r.json().then((v) => {
               var comments = v.comments;
               var container = document.getElementById("container-comment");
-              var length = comments.length-1
               comments.reverse()
               for (let i in comments) {
                     container.innerHTML +=
@@ -93,9 +41,45 @@ function get() {
   });
 }
 
-darkmode()
+function insert() {
+    const formE1 = document.querySelector('.form');
+
+    formE1.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formData = new FormData(formE1);
+        const data = Object.fromEntries(formData);
+
+        fetch('http://127.0.0.1:5000/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+    });
+    remove()
+    get()
+}
+
+function deletePost(id) {
+    data = {'id': id}
+    fetch('http://127.0.0.1:5000/posts', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    remove()
+    get()
+}
+
+insert()
 get()
 var intervalId = window.setInterval(function(){
     remove()
     get()
-}, 6000);
+}, 60000);
